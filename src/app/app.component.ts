4226734c56e11041fc39516a80f966ab7b4f98cd
Aravind Marri <aravindmarri99@gmail.com>
2025-02-22 18:49:06 -0600

    updated the site and version of angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Social } from './social';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
 })
 export class AppComponent {
   title = 'temp-website';
